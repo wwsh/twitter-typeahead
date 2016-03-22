9205c2402dc5d72dc686f547341b51b8d25b6aa9
Thomas Parys <thomas.parys@wwsh.io>
2016-03-22 10:32:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+twitter-typeahead
+=================
+
+A Symfony project created on March 22, 2016, 9:55 am.
---
+++ src/WW/TwitterTypeaheadBundle/TwitterTypeaheadBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WW\TwitterTypeaheadBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TwitterTypeaheadBundle extends Bundle
+{
+}
